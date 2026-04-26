4c8c4b440d9c46911cc3980f153aac767e808220
Sebastian-R-Acosta <sa23-1806@unphu.edu.do>
2026-04-26 17:13:20 -0400

    Describe your changes

--- push.ps1
+++
@@ -1,4 +0,0 @@
-cd C:\Projects\vehicle-tracker
-"C:\Program Files\Git\bin\git.exe" add .
-"C:\Program Files\Git\bin\git.exe" commit -m "Fix registration API - return proper JSON errors"
-"C:\Program Files\Git\bin\git.exe" push origin main
