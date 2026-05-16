38741b2195c1091a1be6397c4dd86caaba2c8095
Sebastian-R-Acosta <sa23-1806@unphu.edu.do>
2026-05-16 15:27:20 -0400

    Remove accidentally committed debug scripts, update .gitignore

--- check-vehicles.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-prisma.vehicle.findMany({ include: { maintenanceRecords: true, reminders: true } }).then(r => {
-  console.log(JSON.stringify(r, null, 2));
-  prisma.$disconnect();
-}).catch(e => {
-  console.error(e.message);
-  prisma.$disconnect();
-});
--- cleanup.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-prisma.user.deleteMany({ where: { email: "testlogin@test.com" } }).then(r => {
-  console.log("Deleted:", r.count);
-  prisma.$disconnect();
-});
